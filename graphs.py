0305f5b6631626c3ac578c3ad2d4bc688c526a5b
Jpkeys29 <jpmusik29@gmail.com>
2023-07-11 09:59:52 -0400

    graphs 1

--- graphs.py
+++ graphs.py
@@ -2,7 +2,7 @@ from collections import deque
 
 # 1.Depthfirsttraversal(iteratively)
 
-# def dfprint(graph,start):
+# def dft(graph,start):
 #     stack = [start]
 #     while stack:
 #         current = stack[-1]
@@ -20,26 +20,25 @@ from collections import deque
 #         dft(graph,neighbor)
 
 # 3. Breadthfirsttraversal
-def bft(graph,start):
-    queue = deque([start])
-    while queue:
-        current =queue[0]
-        print(current)
-        queue.popleft()
-        for neighbor in graph[current]:
-            queue.append(neighbor)
+# def bft(graph,start):
+#     queue = deque([start])
+#     while queue:
+#         current =queue[0]
+#         print(current)
+#         queue.popleft()
+#         for neighbor in graph[current]:
+#             queue.append(neighbor)
 
 
-graph = {
-    'a': ['b','c'],
-    'b': ['d'],
-    'c': ['e'],
-    'd': ['f'],
-    'e': [],
-    'f': []
-}
+# graph = {
+#     'a': ['b','c'],
+#     'b': ['d'],
+#     'c': ['e'],
+#     'd': ['f'],
+#     'e': [],
+#     'f': []
+# }
 
-bft(graph,'a')
 
 # a -> c
 # |    |
@@ -48,3 +47,33 @@ bft(graph,'a')
 # |    |
 # v    V
 # d <- f 
+
+
+# Write a function, connected_components_count, that takes in the adjacency list of an undirected graph. The function should return the number of connected components within the graph.
+
+def connected_components_count(graph):
+  count = 0
+  visited = set()
+  for current in graph: #It helps to begin a traversal in separate components
+    if helper(graph,current,visited) == True:
+      count +=1
+  return count
+    
+def helper(graph,current,visited): #helper function to fully traverse a component
+  if current in visited:
+    return False
+  visited.add(current)
+  
+  for neighbor in graph[current]:
+    helper(graph,neighbor,visited)
+  return True
+
+# connected_components_count({
+#   0: [8, 1, 5],
+#   1: [0],
+#   5: [0, 8],
+#   8: [0, 5],
+#   2: [3, 4],
+#   3: [2, 4],
+#   4: [3, 2]
+# }) # -> 2
